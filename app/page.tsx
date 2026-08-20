68431a53e394aa6c50c2bc47fda644cab00ab125
Felipe Baz <fbazlevrero@gmail.com>
2026-08-20 19:19:59 -0300

    feat: enable Hero component in Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { ProductosSection } from "@/components/home/ProductosSection";
+import { Hero } from "@/components/home/Hero";
 
 export default function Home() {
   return (
     <main className="flex-1">
-      {/* <Hero /> */}
+      { <Hero /> }
       {/* <Pilares /> */}
       <ProductosSection />
       {/* <Beneficios /> */}
